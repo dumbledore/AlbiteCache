964d20dd98e58c9ec4b6c2f4db2c1fab28ff96be
Svetlin Ankov <galileostudios@gmail.com>
2011-12-19 17:13:55 +0200

    Cache is now correctly a MRU cache. This didn't make the cache much more efficient though

--- src/org/albite/cache/Cache.java
+++ src/org/albite/cache/Cache.java
@@ -98,9 +98,40 @@ public abstract class Cache {
         
         if (item != null) {
             /*
-             * Found it. The getValue() method might do some additional
-             * processing if such is needed, e.g. reading the value data from
-             * a disk or something
+             * Found it
+             */
+
+            if (head != item) {
+                /*
+                 * It's not the first item, bring it to the front then
+                 */
+
+                /*
+                 * The item is not the first one, i.e item.previous is not null
+                 */
+                item.previous.next = item.next;
+
+                if (tail == item) {
+                    /*
+                     * Need to update the tail
+                     */
+                    tail = item.previous;
+                } else {
+                    /*
+                     * There's a next item
+                     */
+                    item.next.previous = item.previous;
+                }
+
+                item.previous = null;
+                item.next = head;
+                head.previous = item;
+                head = item;
+            }
+
+            /*
+             * The getValue() method might do some additional processing if
+             * such is needed, e.g. reading the value data from a disk
              */
             return item.getValue();
         } else {
