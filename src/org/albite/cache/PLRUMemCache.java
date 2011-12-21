39464574b6f5faec706a77815153b53df9cd5bb0
Svetlin Ankov <galileostudios@gmail.com>
2011-12-21 11:50:41 +0200

    Added a pseudo-LRU mem cache implementation

---
+++ src/org/albite/cache/PLRUMemCache.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.albite.cache;
+
+/**
+ *
+ * @author Albus Dumbledore
+ */
+public class PLRUMemCache extends PLRUCache {
+    public PLRUMemCache(
+            final Cache subCache,
+            final int   targetPhysicalSize,
+            final int   physicalCapacity,
+            final int   targetElementSize,
+            final int   elementCapacity) {
+
+        if (targetPhysicalSize <= 0
+                || physicalCapacity <= 0
+                || targetElementSize <= 0
+                || elementCapacity <= 0) {
+
+            throw new IllegalArgumentException("sizes sould be positive");
+        }
+
+        if (targetPhysicalSize > physicalCapacity
+                || targetElementSize > elementCapacity) {
+
+            throw new IllegalArgumentException(
+                    "target cannot be > the capacity");
+        }
+
+        this.subCache           = subCache;
+        this.targetPhysicalSize = targetPhysicalSize;
+        this.physicalCapacity   = physicalCapacity;
+        this.targetElementSize  = targetElementSize;
+        this.elementCapacity    = elementCapacity;
+    }
+
+    @Override
+    protected CacheItem createCacheItem(Object key, Cacheable value) {
+        return new MemCacheItem(key, value);
+    }
+
+    private class MemCacheItem extends CacheItem {
+        Cacheable value;
+
+        MemCacheItem(final Object key, final Cacheable value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        Cacheable getValue() {
+            return value;
+        }
+    }
+}
